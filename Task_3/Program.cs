a76928c5c606c319e86405b9037c827889203591
PendolFF <morev.ecrgo@gmail.com>
2024-03-30 16:08:12 +0700

    end of task3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int[7];
+int[] array1 = new int[array.Length / 2];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write(array[i] + "\t");
+}
+
+Console.WriteLine();
+for (int i = 0; i < array.Length / 2; i++)
+{
+    array1[i] = array[i] * array[array.Length - 1 - i];
+    Console.Write(array1[i] + "\t");
+}
