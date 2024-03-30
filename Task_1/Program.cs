2d623a17a202e2cf27acb200263eeb289b50652b
PendolFF <morev.ecrgo@gmail.com>
2024-03-30 15:00:25 +0700

    end task1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [size];
+int userNum = Convert.ToInt32(Console.ReadLine());
+string Print = "Нет";
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,20);
+    Console.Write(array[i]+ " ");
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (userNum == array[i])
+    {
+        Print = "Да";
+    }
+       
+}
+Console.WriteLine(Print);
