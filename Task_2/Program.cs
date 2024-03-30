d1c522675acad9e7dbd2711e87ee07fc1b760c4e
PendolFF <morev.ecrgo@gmail.com>
2024-03-30 15:19:19 +0700

    end task2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//создание массива из случайных числе утсановленного размера и диапазона элементов
+int[] array = new int [10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10,10);
+    Console.Write(array[i]+ "\t");
+}
+ Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] =-1 * array[i];
+    Console.Write(array[i]+ "\t");
+}
